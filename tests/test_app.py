2fec955c8218a02804700f2a7a27e7a990c4db4b
KaelDucatti <ducatti860@gmail.com>
2025-10-11 21:04:39 -0300

    .

---
+++ tests/test_app.py
@@ -0,0 +1,14 @@
+from http import HTTPStatus
+
+from fastapi.testclient import TestClient
+
+from app.app import app
+
+
+def test_root_return_ok_and_message():
+    client = TestClient(app)  # arrange
+
+    response = client.get("/")  # act
+
+    assert response.status_code == HTTPStatus.OK  # assert
+    assert response.json() == {"message": "Hello, FastAPI!"}  # assert
