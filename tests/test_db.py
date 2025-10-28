978aa65b165011c0f8c8376d94a5da08adefe35f
KaelDucatti <ducatti860@gmail.com>
2025-10-27 23:59:19 -0300

    create: 'tests.test_db.test_create_user'

--- tests/test_db.py
+++ tests/test_db.py
@@ -1,7 +1,10 @@
 from app.models import User
 
 
-def test_create_user():
+def test_create_user(session):
     user = User(username="alice", email="alice@example.com", password="secret")
+
+    session.add(user)
+    session.commit()
 
     assert user.password == "secret"
