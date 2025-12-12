4018e931eec7b12350140ae345be9cd223462d3d
KaelDucatti <ducatti860@gmail.com>
2025-12-11 23:04:26 -0300

    add: UserPublicSchema/UserPrivateSchema

--- my_own_list/app.py
+++ my_own_list/app.py
@@ -2,7 +2,7 @@ from http import HTTPStatus
 
 from fastapi import FastAPI
 
-from my_own_list.schemas import Message
+from my_own_list.schemas import Message, UserPrivateSchema, UserPublicSchema
 
 app = FastAPI()
 
@@ -10,3 +10,10 @@ app = FastAPI()
 @app.get('/', status_code=HTTPStatus.OK, response_model=Message)
 def read_root():
     return {'message': 'Hello, FastAPI'}
+
+
+@app.post(
+    '/user/', status_code=HTTPStatus.CREATED, response_model=UserPublicSchema
+)
+def create_user(user: UserPrivateSchema):
+    return user
